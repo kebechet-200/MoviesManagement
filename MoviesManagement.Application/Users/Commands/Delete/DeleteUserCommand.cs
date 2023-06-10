41862ca1723778d960e206473f67a1f02663d273
Victus_HP <lashasuliash@gmail.com>
2023-06-10 23:35:06 +0200

    fix error

--- MoviesManagement.Application/Users/Commands/Delete/DeleteUserCommand.cs
+++ MoviesManagement.Application/Users/Commands/Delete/DeleteUserCommand.cs
@@ -2,5 +2,5 @@
 
 namespace MoviesManagement.Application.Users.Commands.Delete
 {
-    public record DeleteUserCommand(Guid id) : IRequest<Unit>
+    public record DeleteUserCommand(Guid id) : IRequest<Unit>;
 }
