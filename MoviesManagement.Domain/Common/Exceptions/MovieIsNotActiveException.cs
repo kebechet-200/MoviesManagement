cf69ccb2781ed72c67cb7dbf47b59174e374ab2a
Victus_HP <lashasuliash@gmail.com>
2023-06-10 23:31:28 +0200

    fix logic error, add exception

---
+++ MoviesManagement.Domain/Common/Exceptions/MovieIsNotActiveException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesManagement.Domain.Common.Exceptions
+{
+    public class MovieIsInactiveException : Exception
+    {
+        public const string Code = "The movie is inactive";
+
+        public MovieIsInactiveException(string message) : base(message) { }
+    }
+}
