e986070a3f2b61f4afb8159c139e356d4cafacf1
Victus_HP <lashasuliash@gmail.com>
2023-06-11 14:20:02 +0200

    add cancel ticket business model

--- MoviesManagement.Application/Tickets/Commands/Cancel/CancelTicketCommandHandler.cs
+++ MoviesManagement.Application/Tickets/Commands/Cancel/CancelTicketCommandHandler.cs
@@ -1,6 +1,64 @@
-﻿namespace MoviesManagement.Application.Tickets.Commands.Cancel
+﻿using MediatR;
+using MoviesManagement.Application.Common.Extensions;
+using MoviesManagement.Application.Contracts;
+using MoviesManagement.Domain.Common.Enum;
+using MoviesManagement.Domain.Common.Exceptions;
+
+namespace MoviesManagement.Application.Tickets.Commands.Cancel
 {
-    internal class CancelTicketCommandHandler
+    public class CancelTicketCommandHandler : IRequestHandler<CancelTicketCommand, Unit>
     {
+        private readonly ITicketRepository _ticketRepository;
+        private readonly IMovieRepository _movieRepository;
+        private readonly IUserRepository _userRepository;
+
+        public CancelTicketCommandHandler(ITicketRepository ticketRepository,
+                                          IMovieRepository movieRepository,
+                                          IUserRepository userRepository)
+        {
+            _ticketRepository = ticketRepository;
+            _movieRepository = movieRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<Unit> Handle(CancelTicketCommand request, CancellationToken cancellationToken)
+        {
+            if (request.UserId == Guid.Empty)
+                throw new TicketNotFoundException("Userid is empty");
+
+            if (request.MovieId == Guid.Empty)
+                throw new TicketNotFoundException("MovieId is empty");
+
+            if (request.State is not TicketEnum.Cancel)
+                throw new InvalidStateException("Ticket status is not Cancel");
+
+            var user = await _userRepository.GetAsync(request.UserId).ConfigureAwait(false);
+            var movie = await _movieRepository.GetAsync(request.MovieId).ConfigureAwait(false);
+
+            if (user is null)
+                throw new UserDoesNotExistException($"User with an id {request.UserId} does not exist in the database");
+
+            if (movie is null)
+                throw new MoviesNotFoundException($"Movie with an id {request.MovieId} does not exist in the database");
+
+            if (movie.IsActive is false)
+                throw new MovieIsInactiveException("The ticket can't be cancelled because the movie is inactive");
+
+            bool isLessThanHourFromStart = DateTime.UtcNow > movie.StartDate.AddHours(-1);
+
+            if (isLessThanHourFromStart)
+                throw new MovieStartsLessThanAnHourException("You can't cancel the movie starts less than an hour.");
+
+            var movieTickets = user.Tickets
+                .Where(x => x.UserId == user.Id)
+                .Where(x => x.MovieId == movie.Id);
+
+            if (movieTickets.Any(x => x.State is not TicketEnum.Cancel))
+                throw new CantCancelTicketException("You don't have active tickets");
+
+            await _ticketRepository.CancelTicketAsync(request.TicketCommandToDomain()).ConfigureAwait(false);
+
+            return Unit.Value;
+        }
     }
 }
---
+++ MoviesManagement.Domain/Common/Exceptions/CantCancelTicketException.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesManagement.Domain.Common.Exceptions
+{
+    public class CantCancelTicketException : Exception
+    {
+        public const string Code = "You can't cancel the ticket";
+
+        public CantCancelTicketException(string message) : base(message) { }
+    }
+}
