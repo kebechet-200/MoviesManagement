b751ec285e1e262762ae4f82883a9c320f9585dd
Victus_HP <lashasuliash@gmail.com>
2023-06-15 23:51:22 +0200

    fix user exception

---
+++ MoviesManagement.Domain/Common/Exceptions/UserIdIsEmptyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesManagement.Domain.Common.Exceptions
+{
+    public class UserIdIsEmptyException : Exception
+    {
+        public const string Code = "User id is empty";
+
+        public UserIdIsEmptyException(string message) : base(message) { }
+    }
+}
