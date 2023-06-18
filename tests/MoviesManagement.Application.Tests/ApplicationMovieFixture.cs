f844b1866162175e97cba634ee35e497fd142c39
Victus_HP <lashasuliash@gmail.com>
2023-06-18 18:13:26 +0200

    add movie fixture

---
+++ tests/MoviesManagement.Application.Tests/ApplicationMovieFixture.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using MoviesManagement.Application.Contracts;
+
+namespace MoviesManagement.Application.Tests
+{
+    public class ApplicationMovieFixture
+    {
+        private readonly Mock<IMovieRepository> _movieRepository;
+        public ApplicationMovieFixture()
+        {
+            _movieRepository = new Mock<IMovieRepository>();
+
+            _movieRepository.Setup(x => )
+        }
+    }
+}
